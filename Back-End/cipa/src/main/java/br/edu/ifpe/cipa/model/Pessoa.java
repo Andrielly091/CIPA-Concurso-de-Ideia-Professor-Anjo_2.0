953633f50cc6edd50e44560360303ebf558195ce
Jadeilson Martinho <75817805+jadeilsonm@users.noreply.github.com>
2022-05-11 10:33:53 -0300

    Fix: CRUD Pessoas

--- Back-End/cipa/src/main/java/br/edu/ifpe/cipa/model/Pessoa.java
+++ Back-End/cipa/src/main/java/br/edu/ifpe/cipa/model/Pessoa.java
@@ -2,40 +2,34 @@ package br.edu.ifpe.cipa.model;
 
 public class Pessoa {
 	
-	private int codigo;
 	private String nome;
-	private String endereco;
-	private String cidade;
-	private String Uf;
-	public int getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
+	private String email;
+	private String senha;
+	private String tipo;
+	
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getEndereco() {
-		return endereco;
+	public String getEmail() {
+		return email;
 	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	public String getCidade() {
-		return cidade;
+	public String getSenha() {
+		return senha;
 	}
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
-	public String getUf() {
-		return Uf;
+	public String getTipo() {
+		return tipo;
 	}
-	public void setUf(String uf) {
-		Uf = uf;
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 	
 	
