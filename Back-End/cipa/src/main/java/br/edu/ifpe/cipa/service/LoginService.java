a04098bdecd40810cfcff9ff0c88513d20248a0c
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-06-08 00:51:44 -0300

    feat: findByEmail

--- Back-End/cipa/src/main/java/br/edu/ifpe/cipa/service/LoginService.java
+++ Back-End/cipa/src/main/java/br/edu/ifpe/cipa/service/LoginService.java
@@ -3,45 +3,16 @@ package br.edu.ifpe.cipa.service;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
-import br.edu.ifpe.cipa.dao.LoginDao;
 import br.edu.ifpe.cipa.dao.PessoaDao;
-import br.edu.ifpe.cipa.model.Login;
 import br.edu.ifpe.cipa.model.Pessoa;
 
 @Service
 public class LoginService {
-	
-	private final
 		
-	LoginDao logindao = new LoginDao();
 	
 	PessoaDao pessoas = new PessoaDao();
-	
-	
-	public List<Login> listar() throws ClassNotFoundException, SQLException {
-		return logindao.listar();
-	}
-	
-	public Login alterar(Login loginBd) throws ClassNotFoundException, SQLException {
-		logindao.alterar(loginBd);
-		return loginBd;
-		
-	}
-	
-	public void inserir(Login login) throws ClassNotFoundException, SQLException {
-		logindao.inserir(login);
-	}
-	
-	public void remover(int loginId) throws ClassNotFoundException, SQLException {
-		logindao.remover(loginId);
-	}
 
-	public void sendEmail() {
-		
-	}
 	
 	public Pessoa auth(String email, String senha) throws SQLException {
 		
