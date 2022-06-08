1e5ad342b1071b2756bffaecc4a7117930326ec8
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-06-08 14:06:26 -0300

    feat: serviço de email

---
+++ Back-End/email/src/main/java/br/edu/ifpe/email/model/Email.java
@@ -0,0 +1,42 @@
+package br.edu.ifpe.email.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Email {
+
+    public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	private String from;
+    private String to;
+    private String subject;
+    private String text;
+}
